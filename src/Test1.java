57ae681991c466fb1bf5076fd09133920c6fea0c
ashok.chaduvu <ashok.chaduvu@BDC6-LX-86TXMW2.dir.svc.accenture.com>
2021-01-29 12:27:27 +0530

    First commit......

---
+++ src/Test1.java
@@ -0,0 +1,14 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String text = "yes";
+		if (text == "text") {
+			System.out.println("correct");
+		}else {
+			System.out.println("No");
+		}
+	}
+
+}
